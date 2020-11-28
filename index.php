532e58df12e08584d86cb9760b2e044f087d6933
rioil <rioil.dev@gmail.com>
2020-11-28 21:17:26 +0900

    api呼び出しログ記録処理を追加

--- index.php
+++ index.php
@@ -53,6 +53,24 @@ header("Content-Type: application/json; charset=utf-8");
 header("Access-Control-Allow-Origin: *");
 echo json_encode($json, JSON_UNESCAPED_UNICODE);
 
+// log追加
+$now = (new DateTime())->format('Y-m-d H:i:s');
+$sender_ip = inet_pton($_SERVER['REMOTE_ADDR']);
+$referer = $_SERVER['HTTP_REFERER'];
+$args = $_SERVER["QUERY_STRING"];
+
+$sql = 'INSERT INTO api_log VALUES (default, :now, :sender_ip, :referer, :args)';
+$stmt = $db->prepare($sql);
+$stmt->bindValue(':now',        $now,       PDO::PARAM_STR);
+$stmt->bindValue(':sender_ip',  $sender_ip, PDO::PARAM_STR);
+$stmt->bindValue(':referer',    $referer,   PDO::PARAM_STR);
+$stmt->bindValue(':args',       $args,      PDO::PARAM_STR);
+
+var_dump($stmt->execute());
+echo($now);
+var_dump($sender_ip);
+echo($args);
+
 /**
  * 文字列のDateTime型への変換を試みます
  */
